2ee843effefa8107677ba463d95d6c5686e1adc4
h4fizm <farelhafiz.fhm@gmail.com>
2025-08-13 17:19:44 +0700

    modification on storage data on localstorage

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// index.html - File js/script.js
+
 // INDEX FUNCTION
 AOS.init();
 
@@ -5,38 +7,32 @@ AOS.init();
 const form = document.getElementById("dataForm");
 
 form.addEventListener("submit", function (event) {
-  // Mencegah form untuk melakukan reload halaman
   event.preventDefault();
 
-  // Mengambil nilai dari setiap input
   const unitLoader = document.getElementById("unitLoader").value;
   const jenisMaterial = document.getElementById("jenisMaterial").value;
   const namaOperator = document.getElementById("namaOperator").value;
   const observer = document.getElementById("observer").value;
 
-  // Validasi form
   if (
     unitLoader === "UNIT LOADER" ||
     jenisMaterial === "JENIS MATERIAL" ||
     namaOperator === "NAMA OPERATOR" ||
     observer === ""
   ) {
-    // Tampilkan SweetAlert jika ada form yang kosong
     Swal.fire({
       icon: "error",
       title: "Gagal",
       text: "Semua form harus diisi!",
     });
-    return; // Hentikan eksekusi kode selanjutnya
+    return;
   }
 
-  // Membuat objek JavaScript
   const data = {
     unit_loader: unitLoader,
     jenis_material: jenisMaterial,
     nama_operator: namaOperator,
     observer: observer,
-    // [2025-08-11] Tanggal pengajuan dalam format hari, tanggal bulan tahun.
     tanggal_pengajuan: new Date().toLocaleDateString("id-ID", {
       weekday: "long",
       year: "numeric",
@@ -45,28 +41,23 @@ form.addEventListener("submit", function (event) {
     }),
   };
 
-  // Mengubah objek menjadi string JSON
   const jsonData = JSON.stringify(data);
-
-  // Menyimpan string JSON ke Local Storage
   localStorage.setItem("formData", jsonData);
 
-  console.log("Data berhasil disimpan di Local Storage:", jsonData);
+  // Menghapus riwayat proses sebelumnya untuk memulai sesi baru
+  localStorage.removeItem("processHistory");
 
-  // Tampilkan SweetAlert jika berhasil
   Swal.fire({
     icon: "success",
     title: "Berhasil!",
     text: "Data berhasil disimpan.",
     showConfirmButton: false,
-    timer: 1500, // SweetAlert akan tertutup otomatis setelah 1.5 detik
+    timer: 1500,
   }).then(() => {
-    // Arahkan ke halaman selanjutnya setelah SweetAlert tertutup
     window.location.href = "index2.html";
   });
 });
 
-// Sembunyikan preloader setelah halaman selesai dimuat
 window.addEventListener("load", function () {
   const preloader = document.getElementById("preloader");
   if (preloader) {
